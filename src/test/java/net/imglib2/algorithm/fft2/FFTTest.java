cdc6ea11f852300939ff311502dfd166c01e3d50
Brian Northan <bnorthan@gmail.com>
2015-09-07 23:02:20 -0400

    profile code

--- src/test/java/net/imglib2/algorithm/fft2/FFTTest.java
+++ src/test/java/net/imglib2/algorithm/fft2/FFTTest.java
@@ -34,6 +34,9 @@
 package net.imglib2.algorithm.fft2;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import net.imglib2.Cursor;
 import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
@@ -64,11 +67,11 @@ import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
  * @author Curtis Rueden
  * @author Brian Northan
  */
-public class FFTTest /* extends /*AbstractBenchmark */{
+public class FFTTest {
 
 	/**
-	 * Test basic FFT functionality by performing FFT followed by inverse FFT
-	 * then asserting that the original image has been recovered
+	 * Test basic FFT functionality by performing FFT followed by inverse FFT then
+	 * asserting that the original image has been recovered
 	 */
 	@Test
 	public void testFFT() {
@@ -77,22 +80,22 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			long[] dim = new long[] { i, i, i };
 
 			Img<FloatType> input = new ArrayImgFactory<FloatType>().create(dim,
-					new FloatType());
+				new FloatType());
 			placeSphereInCenter(input);
 
 			ImgFactory<ComplexFloatType> fftImgFactory = null;
 
 			try {
-				fftImgFactory = input.factory().imgFactory(
-						new ComplexFloatType());
-			} catch (IncompatibleTypeException ex) {
+				fftImgFactory = input.factory().imgFactory(new ComplexFloatType());
+			}
+			catch (IncompatibleTypeException ex) {
 				fftImgFactory = null;
 			}
 
 			Img<ComplexFloatType> fft = FFT.realToComplex(input, fftImgFactory);
 
-			Img<FloatType> inverse = input.factory().create(
-					new FinalDimensions(dim), new FloatType());
+			Img<FloatType> inverse = input.factory().create(new FinalDimensions(dim),
+				new FloatType());
 			FFT.complexToRealUnpad(fft, inverse);
 
 			assertImagesEqual(input, inverse, 0.00005f);
@@ -104,22 +107,21 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 	 */
 	@Test
 	public void testJTransformVsMines() {
+		
+		int trials=1000;
 
-		for (int i = 40; i < 100; i++) {
+		for (int i = 50; i < 1048; i++) {
 
-			Dimensions unpaddedDimensions = new FinalDimensions(new long[] { i,
-					i });
+			Dimensions unpaddedDimensions = new FinalDimensions(new long[] { i, i });
 
 			long[] paddedDimensionsMines = new long[2];
 			long[] fftDimensionsMines = new long[2];
 
 			// use the "mines" version of FFTMethods to calculate the dimensions
-			net.imglib2.algorithm.fft2.mines.FFTMethods
-					.dimensionsRealToComplexFast(unpaddedDimensions,
-							paddedDimensionsMines, fftDimensionsMines);
+			net.imglib2.algorithm.fft2.mines.FFTMethods.dimensionsRealToComplexFast(
+				unpaddedDimensions, paddedDimensionsMines, fftDimensionsMines);
 
-			Dimensions dimensionsMines = new FinalDimensions(
-					paddedDimensionsMines);
+			Dimensions dimensionsMines = new FinalDimensions(paddedDimensionsMines);
 
 			long[] paddedDimensionsJTransform = new long[2];
 			long[] fftDimensionsJTransform = new long[2];
@@ -127,16 +129,14 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			// use the 'jtransform' version of FFTMethods to confirm that
 			// jtransform supports the dimensions
 			FFTMethods.dimensionsRealToComplexSmall(dimensionsMines,
-					paddedDimensionsJTransform, fftDimensionsJTransform);
+				paddedDimensionsJTransform, fftDimensionsJTransform);
 
 			Dimensions dimensions = new FinalDimensions(paddedDimensionsMines);
 
 			// assert that the mines dimensions and jtransform dimensions are
 			// equal
-			assertEquals(paddedDimensionsMines[0],
-					paddedDimensionsJTransform[0]);
-			assertEquals(paddedDimensionsMines[1],
-					paddedDimensionsJTransform[1]);
+			assertEquals(paddedDimensionsMines[0], paddedDimensionsJTransform[0]);
+			assertEquals(paddedDimensionsMines[1], paddedDimensionsJTransform[1]);
 
 			// using the above calculated dimensions to form an input array
 
@@ -155,34 +155,133 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 
 			// create images for input and transform
 			Img<FloatType> inJTransform = ArrayImgs.floats(array,
-					paddedDimensionsMines);
-			Img<FloatType> inMines = ArrayImgs.floats(array2,
-					paddedDimensionsMines);
+				paddedDimensionsMines);
+			Img<FloatType> inMines = ArrayImgs.floats(array2, paddedDimensionsMines);
 
-			Img<ComplexFloatType> transformJTransform = new ArrayImgFactory<ComplexFloatType>()
-					.create(fftDimensionsMines, new ComplexFloatType());
-			Img<ComplexFloatType> transformMines = new ArrayImgFactory<ComplexFloatType>()
-					.create(fftDimensionsMines, new ComplexFloatType());
-
-			// perform forward and inverse fft using the FFTMethods (which uses
-			// jtransform)
+			Img<ComplexFloatType> transformJTransform =
+				new ArrayImgFactory<ComplexFloatType>().create(fftDimensionsMines,
+					new ComplexFloatType());
+			Img<ComplexFloatType> transformMines =
+				new ArrayImgFactory<ComplexFloatType>().create(fftDimensionsMines,
+					new ComplexFloatType());
+			
 			FFTMethods.realToComplex(inJTransform, transformJTransform, 0);
 
-			// perform forward and inverse fft with the legacy FFTMethods which
-			// uses mines
-			net.imglib2.algorithm.fft2.mines.FFTMethods.realToComplex(inMines,
+			long start = System.currentTimeMillis();
+
+			for (int j = 0; j < trials; j++) {
+				// perform forward and inverse fft using the FFTMethods (which uses
+				// jtransform)
+				FFTMethods.realToComplex(inJTransform, transformJTransform, 0);
+			}
+
+			long jTransformTime = System.currentTimeMillis() - start;
+
+			System.out.println();
+			System.out.println("i: " + i);
+			System.out.println("Jtransform time: " + jTransformTime);
+
+			start = System.currentTimeMillis();
+
+			for (int j = 0; j < trials; j++) {
+				// perform forward and inverse fft with the legacy FFTMethods which
+				// uses mines
+				net.imglib2.algorithm.fft2.mines.FFTMethods.realToComplex(inMines,
 					transformMines, 0);
+			}
+
+			long minesTime = System.currentTimeMillis() - start;
+
+			System.out.println("Mines time: " + minesTime);
+			System.out.println();
 
 			// assert that the images are the same
-			assertComplexImagesEqual(transformJTransform, transformMines,
-					0.0001f);
+			assertComplexImagesEqual(transformJTransform, transformMines, 0.0001f);
 
 		}
 	}
 
+	@Test
+	public void testFFTMethodsVsJTransformAPIBenchmark() {
+
+		int startSize = 90;
+		int finishSize = 110;
+		int incr=1;
+		
+		// number of trials to perform for each size
+		int trials = 1000;
+		
+		// a list of sizes for which jTransform is faster
+		ArrayList<Integer> jTransformFaster=new ArrayList<Integer>();
+		
+		// a list of sizes for which jTransform is 10% of mines
+		ArrayList<Integer> jTransformWithinX=new ArrayList<Integer>();
+		Double X=1.10;
+		
+		for (int size = startSize; size < finishSize; size+=incr) {
+
+			int jTransformSize = NextSmoothNumber.nextSmooth(size);
+			float[] array = createTestArray(jTransformSize);
+
+			int minesSize = FftReal.nfftFast(size);
+			final int complexSize = (minesSize / 2 + 1) * 2;
+
+			float[] minesArrayIn = createTestArray(minesSize);
+			float[] minesArrayOut = new float[complexSize];
+
+			// create and perform forward and inverse fft on the data using
+			// JTransform
+			// directly
+			final FloatFFT_1D fft_jt = new FloatFFT_1D(jTransformSize);
+			
+			final FftReal fft_mines = new FftReal(minesSize);
+
+			long start = System.currentTimeMillis();
+
+			for (int i = 1; i < trials; i++) {
+				minesArrayIn = createTestArray(minesSize);
+				fft_mines.realToComplex(1, minesArrayIn, minesArrayOut);
+				//fft_mines.complexToReal(1, minesArrayOut, minesArrayIn);
+			}
+
+			long minesTime = System.currentTimeMillis() - start;
+
+			start = System.currentTimeMillis();
+			
+			fft_jt.realForward(array);
+			fft_jt.realInverse(array, true);
+
+			for (int i = 1; i < trials; i++) {
+				array = createTestArray(jTransformSize);
+				fft_jt.realForward(array);
+				//fft_jt.realInverse(array, true);
+			}
+
+			long jTransformTime = System.currentTimeMillis() - start;
+
+			System.out.println("size: " + size);
+			System.out.println("jtransform size: "+jTransformSize+" mines size: "+minesSize);
+			System.out.println("JTransform time: " + jTransformTime);
+			System.out.println("Mines time: " + minesTime);
+			
+			if (jTransformTime<minesTime) {
+				jTransformFaster.add(size);
+			}
+			else if (jTransformTime<X*minesTime) {
+				jTransformWithinX.add(size);
+			}
+		}
+		
+		System.out.println("jTransform faster: ");
+		System.out.println(jTransformFaster);
+		
+		System.out.println("jTransform within "+X);
+		System.out.println(jTransformWithinX);
+	}
+
 	/**
-	 * This test confirms that the result using FFTMethods is equal to the
-	 * result obtained using the JTransform api
+	 * This test confirms that the result using FFTMethods is equal to the result
+	 * obtained using the JTransform api
 	 */
 	@Test
 	public void testFFTMethodsVsJTransformAPI() {
@@ -190,13 +289,12 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 		for (int i = 40; i < 100; i++) {
 
 			// determine the dimensions that FFTMethods will use given i
-			Dimensions unpaddedDimensions = new FinalDimensions(
-					new long[] { i });
+			Dimensions unpaddedDimensions = new FinalDimensions(new long[] { i });
 			long[] paddedDimensions = new long[1];
 			long[] fftDimensions = new long[1];
 
 			FFTMethods.dimensionsRealToComplexSmall(unpaddedDimensions,
-					paddedDimensions, fftDimensions);
+				paddedDimensions, fftDimensions);
 			Dimensions dimensions = new FinalDimensions(paddedDimensions);
 
 			// using the above calculated dimensions form an input array
@@ -233,10 +331,10 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			Img<FloatType> in = ArrayImgs.floats(copy, paddedDimensions);
 
 			Img<ComplexFloatType> transform = new ArrayImgFactory<ComplexFloatType>()
-					.create(fftDimensions, new ComplexFloatType());
+				.create(fftDimensions, new ComplexFloatType());
 
 			Img<FloatType> inverse = new ArrayImgFactory<FloatType>().create(
-					dimensions, new FloatType());
+				dimensions, new FloatType());
 
 			// perform forward and inverse fft using the FFTMethods approach
 			FFTMethods.realToComplex(in, transform, 0);
@@ -253,8 +351,8 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 				cinverse.fwd();
 
 				// assert that the inverse = the input within the error delta
-				assertEquals(cin.get().getRealFloat(), cinverse.get()
-						.getRealFloat(), 0.001);
+				assertEquals(cin.get().getRealFloat(), cinverse.get().getRealFloat(),
+					0.001);
 
 				// assert that the inverse obtained using FFTMethods api is
 				// exactly
@@ -281,9 +379,8 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 		long[] paddedDimensionsMines = new long[1];
 		long[] fftDimensionsMines = new long[1];
 
-		net.imglib2.algorithm.fft2.mines.FFTMethods
-				.dimensionsRealToComplexFast(unpaddedDimensions,
-						paddedDimensionsMines, fftDimensionsMines);
+		net.imglib2.algorithm.fft2.mines.FFTMethods.dimensionsRealToComplexFast(
+			unpaddedDimensions, paddedDimensionsMines, fftDimensionsMines);
 
 		// using the above calculated dimensions form an input array
 		int realsize = (int) paddedDimensionsMines[0];
@@ -314,24 +411,25 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 
 		// create images for input, transform
 		Img<FloatType> inJTransform = ArrayImgs.floats(memInMines,
-				paddedDimensionsMines);
-		Img<ComplexFloatType> fftJTransform = new ArrayImgFactory<ComplexFloatType>()
-				.create(fftDimensionsMines, new ComplexFloatType());
+			paddedDimensionsMines);
+		Img<ComplexFloatType> fftJTransform =
+			new ArrayImgFactory<ComplexFloatType>().create(fftDimensionsMines,
+				new ComplexFloatType());
 
 		Img<FloatType> inMines = ArrayImgs.floats(memInMines,
-				paddedDimensionsMines);
+			paddedDimensionsMines);
 		Img<ComplexFloatType> fftMines = new ArrayImgFactory<ComplexFloatType>()
-				.create(fftDimensionsMines, new ComplexFloatType());
+			.create(fftDimensionsMines, new ComplexFloatType());
 
 		// perform forward and inverse fft using the FFTMethods approach
 		FFTMethods.realToComplex(inJTransform, fftJTransform, 0);
 
-		net.imglib2.algorithm.fft2.mines.FFTMethods.realToComplex(inMines,
-				fftMines, 0);
+		net.imglib2.algorithm.fft2.mines.FFTMethods.realToComplex(inMines, fftMines,
+			0);
 
 		for (int j = 0; j < realsize; j++) {
-			System.out.println("j: " + j + " jt: " + memJTransform[j]
-					+ " mines: " + memOutMines[j]);
+			System.out.println("j: " + j + " jt: " + memJTransform[j] + " mines: " +
+				memOutMines[j]);
 
 		}
 
@@ -348,15 +446,15 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			cmines.fwd();
 
 			System.out.println();
-			System.out.println("jt: " + cjt.get().getRealFloat() + " "
-					+ cjt.get().getImaginaryFloat());
-			System.out.println("mines: " + cmines.get().getRealFloat() + " "
-					+ cmines.get().getImaginaryFloat());
+			System.out.println("jt: " + cjt.get().getRealFloat() + " " + cjt.get()
+				.getImaginaryFloat());
+			System.out.println("mines: " + cmines.get().getRealFloat() + " " + cmines
+				.get().getImaginaryFloat());
 
 			assertEquals(cjt.get().getRealFloat(), cmines.get().getRealFloat(),
-					0.0001);
+				0.0001);
 			assertEquals(cjt.get().getImaginaryFloat(), cmines.get()
-					.getImaginaryFloat(), 0.0001);
+				.getImaginaryFloat(), 0.0001);
 		}
 
 		// finally also do a hard coded test
@@ -391,11 +489,11 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 	 * 
 	 * @param img1
 	 * @param img2
-	 * 
 	 * @param delta
 	 */
 	protected void assertImagesEqual(Img<FloatType> img1, Img<FloatType> img2,
-			float delta) {
+		float delta)
+	{
 		Cursor<FloatType> c1 = img1.cursor();
 		Cursor<FloatType> c2 = img2.cursor();
 
@@ -405,15 +503,15 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			c2.fwd();
 
 			// assert that the inverse = the input within the error delta
-			assertEquals(c1.get().getRealFloat(), c2.get().getRealFloat(),
-					delta);
+			assertEquals(c1.get().getRealFloat(), c2.get().getRealFloat(), delta);
 		}
 
 	}
 
 	// a utility to assert that two images are equal
 	protected void assertComplexImagesEqual(Img<ComplexFloatType> img1,
-			Img<ComplexFloatType> img2, float delta) {
+		Img<ComplexFloatType> img2, float delta)
+	{
 		Cursor<ComplexFloatType> c1 = img1.cursor();
 		Cursor<ComplexFloatType> c2 = img2.cursor();
 
@@ -426,11 +524,10 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 			i++;
 
 			// assert that the inverse = the input within the error delta
-			assertEquals(c1.get().getRealFloat(), c2.get().getRealFloat(),
-					delta);
+			assertEquals(c1.get().getRealFloat(), c2.get().getRealFloat(), delta);
 			// assert that the inverse = the input within the error delta
-			assertEquals(c1.get().getImaginaryFloat(), c2.get()
-					.getImaginaryFloat(), delta);
+			assertEquals(c1.get().getImaginaryFloat(), c2.get().getImaginaryFloat(),
+				delta);
 		}
 
 	}
@@ -447,12 +544,22 @@ public class FFTTest /* extends /*AbstractBenchmark */{
 		for (int d = 0; d < img.numDimensions(); d++)
 			center.setPosition(img.dimension(d) / 2, d);
 
-		HyperSphere<FloatType> hyperSphere = new HyperSphere<FloatType>(img,
-				center, 2);
+		HyperSphere<FloatType> hyperSphere = new HyperSphere<FloatType>(img, center,
+			2);
 
 		for (final FloatType value : hyperSphere) {
 			value.setReal(1);
 		}
+	}
+
+	private float[] createTestArray(int size) {
+		final float[] array = new float[size];
+
+		// place a couple of impulses in the array
+		array[size / 4] = 50;
+		array[size / 2] = 100;
+		
+		return array;
 	}
 
 }
